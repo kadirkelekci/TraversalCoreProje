611778b1edcf92e6b7a8e21c312694439200ab2a
Abdülkadir Kelekçi <kadirkelekci@gmail.com>
2024-02-07 09:49:39 +0300

    Add project files.

---
+++ TraversalCoreProje/Models/UserRegisterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Models
+{
+    public class UserRegisterViewModel
+    {
+        [Required(ErrorMessage ="Lütfen Adınızı Giriniz")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Soyadınızı Giriniz")]
+        public string Surname { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Kullanıcı Adınızı Giriniz")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Mail Adresinizi Giriniz")]
+        public string Mail { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifreyi Giriniz")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Lütfen Şifreyi Tekrar Giriniz")]
+        [Compare("Password",ErrorMessage ="Şifreler Uyuşmuyor")]
+        public string ConfirmPassword { get; set; }
+    }
+}
