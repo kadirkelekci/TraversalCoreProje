783d725b8b9298bb8d8f78d7a0465e3f79196b43
Abdülkadir Kelekçi <kadirkelekci@gmail.com>
2024-04-24 08:54:47 +0300

    update240424

---
+++ TraversalCoreProje/Areas/Admin/Controllers/DestinationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalCoreProje.Areas.Admin.Controllers
+{
+    public class DestinationController : Controller
+    {
+        [Area("Admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
